59d0bfcb97d4e5ef55564872d43dfe16bb3d4f56
ShiftHomePC <drugzarbest@gmail.com>
2025-07-03 16:48:52 +0200

    commit all

---
+++ test-pwa/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // Workbox je google library ktorá povoluje offline funkcionalitu
+      // a cachovanie súborov
+      workbox: {
+        // Subory na cashnutie
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Testing PWA App",
+        short_name: "PWAapp",
+        description: "Love, bread and roses for all!",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        // Icons - toto je potrebné pre PWA
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
